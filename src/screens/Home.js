9fc11312d8c37f21fa99ff5f90ca0138c2b672ab
SathishSaminathan <Sathishcse1975@gmail.com>
2019-02-27 14:28:05 +0530

    Initial_commit_for_Map_Integeration

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      
+    };
   }
 
   render() {
@@ -16,12 +19,29 @@ export default class Home extends Component {
           alignItems: "center"
         }}
       >
-        <Text> Home </Text>
-        <Button
-          title="Go to Videos"
-          onPress={() => this.props.navigation.navigate("Videos")}
+        <MapView
+          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
         />
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
