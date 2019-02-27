903952f4364e017ead0ac4b2566896803e999145
SathishSaminathan <Sathishcse1975@gmail.com>
2019-02-27 18:56:48 +0530

    commit_for_adding_map_markers_and_added_two_locations

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -3,7 +3,8 @@ const images = {
   Male: require("./male.png"),
   MaleSelected: require("./maleselected.png"),
   Female: require("./female.png"),
-  FemaleSelected: require("./femaleselected.png")
+  FemaleSelected: require("./femaleselected.png"),
+  GymMarker:require('./gymmarker.png'),
 };
 
 export default images;
